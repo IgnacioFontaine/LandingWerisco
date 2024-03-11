b4d5260d58e76ed87f05ea41c6c1c3b1fe825fcd
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-11 17:45:39 -0300

    locations

--- client/src/assets/Components/Locations/locations.jsx
+++ client/src/assets/Components/Locations/locations.jsx
@@ -2,7 +2,26 @@ function Locations() {
   
   return (
     <>
-      <h1>Locations</h1>
+      <section>
+        <h2 id="donde-encontrarnos">¿Dónde nos pueden encontrar?</h2>
+          <p>
+            Para conseguir nuestros productos te recomendamos estos lugares, es
+            posible que en almacenes cerca de tu hogar también puedas encontrar
+            nuestro producto!
+          </p>
+          <ul>
+            <li>Super Mami Rodriguez del Busto</li>
+            <li>Super Mami Ruta 20</li>
+            <li>Super Mami Circunvalación</li>
+            <li>Super Mami 60 Cuadras</li>
+            <li>Super Mami Salsipuedes</li>
+            <li>Super Mami Alta Gracia</li>
+            <li>Super Mami San Vicente</li>
+            <li>Tadicor</li>
+            <li>La Reposterita</li>
+            <li>Cipriano</li>
+          </ul>
+      </section>
     </>
   )
 }
