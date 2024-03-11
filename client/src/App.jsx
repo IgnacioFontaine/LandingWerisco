d2bdb9c1035765e60ece97113693503149eb3bc7
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-11 18:16:09 -0300

    importaciones listas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Header from './assets/Components/Header/header'
+import NavBar from './assets/Components/NavBar/navbar'
+import Description from './assets/Components/Description/description'
+import Locations from './assets/Components/Locations/locations'
+import Products from './assets/Components/Products/products'
+import Footer from './assets/Components/Footer/footer'
 
 function App() {
   
   return (
     <>
-      <h1>Werisco Landing</h1>
+      
+      
     </>
   )
 }
