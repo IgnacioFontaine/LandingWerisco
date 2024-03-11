67deb4b9fd4d0c52aeb58b3ac9a091e6a2549e95
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-11 17:44:40 -0300

    description

--- client/src/assets/Components/Description/description.jsx
+++ client/src/assets/Components/Description/description.jsx
@@ -2,7 +2,16 @@ function Description() {
   
   return (
     <>
-      <h1>Description</h1>
+      <section>
+        <h2 id="quienes-somos">¿Quiénes somos?</h2>
+          <p>
+            Somos una empresa familiar con más de 20 años de trayectoria que se dedica
+            a producir y vender merengue, a partir de la materia prima, la
+            fabricación, horneado, envasado y distribución. Podés encontrar nuestro
+            producto en muchos puntos de Córdoba, no realizamos ventas al por menor
+            pero cualquier duda podés consultarnos via Mail o por Whatsapp.
+          </p>
+      </section>
     </>
   )
 }
