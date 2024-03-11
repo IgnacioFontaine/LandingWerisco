9e15fc879bbd99d6b1f9c7cfad16025268871e60
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-11 17:48:04 -0300

    cambio en products

--- client/src/assets/Components/Products/products.jsx
+++ client/src/assets/Components/Products/products.jsx
@@ -2,7 +2,33 @@ function Products() {
   
   return (
     <>
-      <h1>Productsos</h1>
+      <section>
+        <h2 id="productos">Productos</h2>
+        <p>
+          Contamos actualmente con tres diferentes productos disponibles, los mismos
+          son:
+        </p>
+        <br />
+        <table>
+          <thead>
+            <th colSpan="3">Nuestros Productos</th>
+          </thead>
+          <tr>
+            <th colSpan="3">Cantidad</th>
+          </tr>
+          <tr>
+            <td>100 g</td>
+            <td>250 g</td>
+            <td>1 k</td>
+          </tr>
+          <tfoot>
+            <td colSpan="3"><small>Consultar precio en tu Supermercado de preferencia</small></td>
+          </tfoot>
+        </table>
+        <br></br>
+        <p><i>Sin conservantes artificiales, más de 20 años en la industria acompañando tus postres y de tus tardes de mate.</i></p>
+        <br></br>
+      </section>
     </>
   )
 }
