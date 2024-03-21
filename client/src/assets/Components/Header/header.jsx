d3e87ed14ec77a5c709984e29c16e8f671a31431
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-21 12:16:18 -0300

    cambios básicos

--- client/src/assets/Components/Header/header.jsx
+++ client/src/assets/Components/Header/header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <>
       <section className="werisco_header">
-        <h1 className="title">Werisco</h1>
+        <h1 className="title">WERISCO</h1>
         <h3 className="subtitle">Pequeños pedacitos de Cielo</h3>
       </section>
     </>
