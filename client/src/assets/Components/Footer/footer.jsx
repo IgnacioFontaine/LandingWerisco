fc253b4e29a86f29d199f5d5f3f53877ea2ea8d8
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-21 23:12:52 -0300

    footer en proceso

--- client/src/assets/Components/Footer/footer.jsx
+++ client/src/assets/Components/Footer/footer.jsx
@@ -1,16 +1,22 @@
+import "./footer.css"
+
 function Footer() {
   
   return (
-    <>
+    < div className="footer-werisco">
       <section >
-        <h2 id="contactos"><strong>Werisco</strong></h2>
+        <div >
+          <h2 id="contactos" className="footer-title"><strong >Werisco &#169;</strong></h2>
+        </div>
         <p>Contáctanos por cualquiera de las vías que te dejamos aquí!</p>
-        <a href="" target="_blank">Mail</a>
-        <a href="" target="_blank">Whatsapp</a>
-        <a href="https://www.instagram.com/werisco_/" target="_blank">Instagram</a>
-        <a href="https://www.facebook.com/Werisco" target="_blank">FaceBook</a>
+        <div className="footer-link-contenedor">
+          <a href="" target="_blank">Mail</a>
+          <a href="" target="_blank">Whatsapp</a>
+          <a href="https://www.instagram.com/werisco_/" target="_blank" className="footer-link">Instagram</a>
+          <a href="https://www.facebook.com/Werisco" target="_blank" className="footer-link">FaceBook</a>
+        </div>
       </section>
-    </>
+    </ div>
   )
 }
 
