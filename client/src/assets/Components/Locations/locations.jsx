15b548b8a6810fcbcb2d16bbd7262375585a6da7
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-22 14:16:06 -0300

    cambio en locations

--- client/src/assets/Components/Locations/locations.jsx
+++ client/src/assets/Components/Locations/locations.jsx
@@ -1,12 +1,14 @@
 import "./locations.css"
+import merengue_imagen from "../../Image/100grImage.png"
 
-const Places = ({ id, name, ref }) => {
-  <div className="Place">
+
+const PlaceCard = ({ id, name}) => {
+  <div className="Place" key={id}>
         <div className="image-place">
-          <img src={image} alt="Merengue" />
+          {/* <img src={merengue_imagen} alt="Merengue" /> */}
         </div>
-        <div className="name-place">
-        <h2>{name}</h2>
+        <div >
+          <h2 className="name-place">{name}</h2>
         </div>
   </div>
 }
@@ -14,26 +16,33 @@ const Places = ({ id, name, ref }) => {
 
 function AllPlaces() {
   const places = [
-    { id: 4, name: "Super Mami Rodriguez del Busto", ref: "" },
-    { id: 5, name: "Super Mami Ruta 20", ref: "" },
-    { id: 6, name: "Super Mami Circunvalación", ref: "" },
-    { id: 7, name: "Super Mami 60 Cuadras", ref: "" },
-    { id: 8, name: "Super Mami Salsipuedes", ref: "" },
-    { id: 9, name: "Super Mami Alta Gracia", ref: "" },
-    { id: 10, name: "Super Mami San Vicente", ref: "" },
-    { id: 11, name: "Tadicor", ref: "" },
-    { id: 12, name: "La Reposterita", ref: "" },
-    { id: 13, name: "Cipriano", ref: "" },
-    { id: 14, name: "Gloria", ref: "" },
-    { id: 15, name: "", ref: "" },]
+    { id: 4, name: "Super Mami Rodriguez del Busto" },
+    { id: 5, name: "Super Mami Ruta 20" },
+    { id: 6, name: "Super Mami Circunvalación" },
+    { id: 7, name: "Super Mami 60 Cuadras" },
+    { id: 8, name: "Super Mami Salsipuedes" },
+    { id: 9, name: "Super Mami Alta Gracia" },
+    { id: 10, name: "Super Mami San Vicente" },
+    { id: 11, name: "Tadicor" },
+    { id: 12, name: "La Reposterita" },
+    { id: 13, name: "Cipriano" },
+    { id: 14, name: "Gloria" },
+    { id: 15, name: "" },]
   
   return (
     <div >
-      <section id="productos"  >
-        {places.map(place =>
-          <Places key={place.id} name={place.name} ref={place.image} />)}
+      <section id="productos" className="places-contenedor"  >
+        {/* {places.map(place =>
+          <PlaceCard key={place.id} name={place.name} />)} */}
+        <div className="Place" key={places[0].id}>
+        <div className="image-place">
+          <img src={merengue_imagen} alt="Merengue" />
+        </div>
+        <div >
+          <h2 className="name-place">{places[0].name}</h2>
+        </div>
+  </div>
       </section>
-      
     </div>
   )
 }
@@ -49,7 +58,10 @@ function Locations() {
             Para conseguir nuestros productos te recomendamos estos lugares, es
             posible que en almacenes cerca de tu hogar también puedas encontrar
             nuestro producto!
-          </small>
+        </small>
+        <div>
+          <AllPlaces />
+        </div>
       </section>
     </>
   )
