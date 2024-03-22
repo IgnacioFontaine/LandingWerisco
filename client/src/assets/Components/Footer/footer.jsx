d2c6e5247d83e56758ee7bce85bd90aa1df3a680
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-22 00:18:05 -0300

    cambios footer

--- client/src/assets/Components/Footer/footer.jsx
+++ client/src/assets/Components/Footer/footer.jsx
@@ -23,19 +23,17 @@ function Footer() {
   
   return (
     < div className="footer-werisco">
-      <section   >
+      <section >
         <div >
-          <h2 id="contactos" className="footer-title"><strong >Werisco &#169;</strong></h2>
+          <h2 id="contactos" className="footer-title"><strong >Werisco</strong></h2>
         </div>
         <div className="footer-link-contenedor">
-          <a href="" target="_blank" ><i className="fa fa-envelope email-icon"></i></a>
-          <a href="" target="_blank" className="fa fa-whatsapp"></a>
+          <a href="marcelofontaine@hotmail.com" target="_blank" ><i className="fa fa-envelope email-icon"></i></a>
+          <a href="https://wa.me/+54 9 3513 85-8318" target="_blank" className="fa fa-whatsapp"></a>
           <a href="https://www.facebook.com/Werisco" target="_blank" className="fa fa-facebook"></a>
           <a href="https://www.instagram.com/werisco_/" target="_blank" className="fa fa-instagram"></a>
+          <h5>Todos los derechos reservados Werisco &#169; --  2024</h5>
         </div>
-      </section>
-      <section>
-        <MailWerisco />
       </section>
     </ div>
   )
