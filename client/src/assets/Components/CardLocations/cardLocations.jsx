7984473ec66d370a55cb8cbed52db0a0d99cbc4d
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-25 11:29:12 -0300

    cards listas

--- client/src/assets/Components/CardLocations/cardLocations.jsx
+++ client/src/assets/Components/CardLocations/cardLocations.jsx
@@ -1,7 +1,7 @@
 import "./cardLocations.css"
 import merengue_imagen from "../../Image/100grImage.png";
 
-const PlaceCard = ({ id, name, direc }) => {
+const PlaceCard = ({ id, name, direc, map_direc }) => {
   
   return (
     <div className="Place" key={id}>
@@ -12,8 +12,8 @@ const PlaceCard = ({ id, name, direc }) => {
           <h2 className="name-place">{name}</h2>
           <h6 className="direc-place">{direc}</h6>
         </div>
-          <div className="map-place">
-            <a  rel="noopener noreferrer" target="_blank"  className="	fa fa-map-marker"></a>
+      <div className="map-place">
+            <a href={map_direc} rel="noopener nomap_direcerrer" target="_blank"  className="fa fa-map-marker"></a>
           </div>
       </div>
   )
@@ -22,27 +22,27 @@ const PlaceCard = ({ id, name, direc }) => {
 
 export default function AllPlaces() {
   const places = [
-    { id: 4, name: "Super Mami Rodriguez del Busto", ref: "https://maps.app.goo.gl/psT52WAkSJvYWn3x6", direc: "Rodríguez del Busto 4086, Córdoba" },
+    { id: 4, name: "Super Mami Rodriguez del Busto", map_direc: "https://maps.app.goo.gl/psT52WAkSJvYWn3x6", direc: "Rodríguez del Busto 4086, Córdoba" },
     
-    { id: 5, name: "Super Mami Ruta 20", ref: "https://maps.app.goo.gl/eTnQs1CRmVHYcfBT6", direc: "Av. Fuerza Aerea Argentina 1700, X5010 Córdoba" },
+    { id: 5, name: "Super Mami Ruta 20", map_direc: "https://maps.app.goo.gl/eTnQs1CRmVHYcfBT6", direc: "Av. Fuerza Aerea Argentina 1700, X5010 Córdoba" },
     
-    { id: 6, name: "Super Mami Circunvalación", ref: "https://maps.app.goo.gl/Vcn2fWbrqrkKobra8", direc: "Planta Baja Ampliacion 7 barrio los olmos, Gondola de Venecia 7, X5097 Córdoba" },
+    { id: 6, name: "Super Mami Circunvalación", map_direc: "https://maps.app.goo.gl/Vcn2fWbrqrkKobra8", direc: "Planta Baja Ampliacion 7 barrio los olmos, Gondola de Venecia 7, X5097 Córdoba" },
     
-    { id: 7, name: "Super Mami 60 Cuadras", ref: "https://maps.app.goo.gl/zUNgZqzLjEXXHbuM6", direc: "Colectora de Circunvalación, X5000 Córdoba" },
+    { id: 7, name: "Super Mami 60 Cuadras", map_direc: "https://maps.app.goo.gl/zUNgZqzLjEXXHbuM6", direc: "Colectora de Circunvalación, X5000 Córdoba" },
     
-    { id: 8, name: "Super Mami Salsipuedes", ref: "https://maps.app.goo.gl/zrUjasr9pBgbn6qo8", direc: "E53 1011, X5113 Córdoba" },
+    { id: 8, name: "Super Mami Salsipuedes", map_direc: "https://maps.app.goo.gl/zrUjasr9pBgbn6qo8", direc: "E53 1011, X5113 Córdoba" },
     
-    { id: 9, name: "Super Mami Alta Gracia", ref: "https://maps.app.goo.gl/zGwxSxeQ6wVHEodN6", direc: "Gabino Ezeiza 1081, RP5, Km 29, Alta Gracia, Córdoba" },
+    { id: 9, name: "Super Mami Alta Gracia", map_direc: "https://maps.app.goo.gl/zGwxSxeQ6wVHEodN6", direc: "Gabino Ezeiza 1081, RP5, Km 29, Alta Gracia, Córdoba" },
     
-    { id: 10, name: "Super Mami San Vicente", ref: "https://maps.app.goo.gl/Sh7qyTzFCtNHFBLW8", direc: "Agustín Garzón 1252B°, X5001 Córdoba" },
+    { id: 10, name: "Super Mami San Vicente", map_direc: "https://maps.app.goo.gl/Sh7qyTzFCtNHFBLW8", direc: "Agustín Garzón 1252B°, X5001 Córdoba" },
     
-    { id: 11, name: "Tadicor", ref: "https://maps.app.goo.gl/YaKCcyXzM2y6bPg99", direc: "colectora Norte, salida 1B, Avenida Circunvalacion, Córdoba" },
+    { id: 11, name: "Tadicor", map_direc: "https://maps.app.goo.gl/YaKCcyXzM2y6bPg99", direc: "colectora Norte, salida 1B, Avenida Circunvalacion, Córdoba" },
     
-    { id: 12, name: "La Reposterita", ref: "https://maps.app.goo.gl/KRxifh6sDH4WoCVg7", direc: "Esquina Rivadavia, Oncativo 101, X5000 Córdoba" },
+    { id: 12, name: "La Reposterita", map_direc: "https://maps.app.goo.gl/KRxifh6sDH4WoCVg7", direc: "Esquina Rivadavia, Oncativo 101, X5000 Córdoba" },
     
-    { id: 13, name: "Cipriano", ref: "https://maps.app.goo.gl/uPf2SMEV8VzRwR3X6", direc: "Fournier 2364, X5011 Córdoba" },
+    { id: 13, name: "Cipriano", map_direc: "https://maps.app.goo.gl/uPf2SMEV8VzRwR3X6", direc: "Fournier 2364, X5011 Córdoba" },
     
-    { id: 14, name: "Gloria", ref:"https://maps.app.goo.gl/9YFkyxVTbmEAUNKn7", direc:"Av. Rafael Núñez 3742, X5009 CFR, Córdoba" }]
+    { id: 14, name: "Gloria", map_direc:"https://maps.app.goo.gl/9YFkyxVTbmEAUNKn7", direc:"Av. Rafael Núñez 3742, X5009 CFR, Córdoba" }]
   
   return (
     <div >
@@ -52,6 +52,7 @@ export default function AllPlaces() {
             key={place.id}
             name={place.name}
             direc={place.direc}
+            map_direc={place.map_direc}
             className="location-card" />)}
       </section>
     </div>
--- client/src/assets/Components/NavBar/navbar.jsx
+++ client/src/assets/Components/NavBar/navbar.jsx
@@ -7,10 +7,10 @@ function NavBar() {
       <ul className="navbar">
         <li><a href="index.html#inicio">Inicio</a></li>
         <li><a href="index.html#quienes-somos">¿Quiénes somos?</a></li>
+        <li><a href="index.html#productos">Productos</a></li>
         <li>
           <a href="index.html#donde-encontrarnos">¿Dónde nos pueden encontrar?</a>
         </li>
-        <li><a href="index.html#productos">Productos</a></li>
         <li><a href="index.html#contactos">Contacto</a></li>
       </ul>
     </>
