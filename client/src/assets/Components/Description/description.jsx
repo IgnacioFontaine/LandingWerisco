0f39f7dd2c38ed5c65a965583a5e5801ab5bbf4b
ignaciofontaine <ignaciofontainebustos@gmail.com>
2024-03-25 13:30:03 -0300

    cambio en description

--- client/src/assets/Components/Description/description.jsx
+++ client/src/assets/Components/Description/description.jsx
@@ -1,4 +1,5 @@
 import "./description.css"
+import logoSinTacc from "../../Image/sinTacc.png";
 
 function Description() {
   
@@ -7,14 +8,17 @@ function Description() {
       <section  id="quienes-somos "  >
         <h2  >¿Quiénes somos?</h2>
           <p className="description-parragraph" >
-            Somos una empresa familiar con más de 20 años de trayectoria que se dedica
-            a producir y vender merengue, a partir de la materia prima, la
-            fabricación, horneado, envasado y distribución. Podés encontrar nuestro
-            producto en muchos puntos de Córdoba, no realizamos ventas al por menor
-            pero cualquier duda podés consultarnos via Mail o por Whatsapp.
+            Somos una pyme familiar con más de 20 años de trayectoria que se dedica
+            a la 
+            fabricación, horneado, envasado y distribución de merengue. Podés encontrar nuestro
+            producto en muchos puntos de Córdoba que detallaremos debajo, no realizamos ventas al por menor
+            pero cualquier duda podés consultarnos via  Mail o por Whatsapp.
+            Todos nuestros productos son sin Tacc, perfectos para sumar dulzura a todos tus días!
         </p>
         <br />
-        
+        <div className="contenedor-image">
+          <img className="description-image" src={logoSinTacc} alt="Sin Tacc" />
+        </div>
       </section>
     </div>
   )
